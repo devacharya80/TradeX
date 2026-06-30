f680565e65f82eedf24ea13bd0bf1367cde8667b
devacharya80 <dhanudeva80@gmail.com>
2026-06-30 13:18:14 +0530

    Home Awards Component Design

--- frontend/src/landingPage/home/Hero.jsx
+++ frontend/src/landingPage/home/Hero.jsx
@@ -3,7 +3,7 @@ import "./homeStyle.css";
 
 function Hero() {
   return (
-    <div className="container p-5 hero">
+    <div className="container p-5 hero mb-5">
       <div className="row text-center">
         <img src={landingHero} alt="hero-img" className="mb-5"/>
         <h1 className="mt-5">Invest in everything</h1>
