aa6008afca785ce51cdc3d774f0a032f1b281a09
devacharya80 <dhanudeva80@gmail.com>
2026-06-30 14:16:51 +0530

    Home Pricing  Component Design

--- frontend/src/landingPage/home/Pricing.jsx
+++ frontend/src/landingPage/home/Pricing.jsx
@@ -1,5 +1,34 @@
 function Pricing() {
-    return ( <h1>pricing</h1> );
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h2>Unbeatable pricing</h2>
+          <p>
+            We pioneered the concept of discount broking and price transparency
+            in India. Flat fees and no hidden charges.
+          </p>
+          <a href="#" className="text-decoration-none">
+            See Pricing<i className="fa-solid fa-angle-right"></i>
+          </a>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-6">
+            <div className="row text-center">
+                <div className="col p-3 border shadow">
+                    <h1 className="mb-3">₹0</h1>
+                    <p>Free equity delivery and <br />direct mutual funds</p>
+                </div>
+                
+                <div className="col p-3 border shadow">
+                    <h1 className="mb-3">₹20</h1>
+                    <p>Intraday and F&O</p>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
