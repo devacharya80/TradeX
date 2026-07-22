c2efbefe2a75152faf55b2030ac47ecdf2567039
devacharya80 <dhanudeva80@gmail.com>
2026-07-22 15:39:49 +0530

    Add Temp Data to Database

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,24 @@ dotenv.config();
 import express from "express";
 
 import { connectToDb } from "./database/db.js";
+// import Holding from "./models/holdingSchema.js";
+// import Position from "./models/positionsSchema.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("hi");
+// });
+
+// app.get("/addholdings", async (req, res) => {
+//   const { positions } = await import("../dashboard/src/data/data.js");
+
+//   const data = await Position.insertMany(positions);
+//   console.log(data);
+
+//   res.send("Inserted");
+// });
 
 connectToDb();
 
